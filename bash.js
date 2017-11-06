8237eb3dfe7f1466a4d06bbf04f952451747fe92
Kevin Ho <ho.kevins@gmail.com>
2017-11-06 13:04:12 -0500

    Lunch 2017-11-06

--- bash.js
+++ bash.js
@@ -1,4 +1,4 @@
-// console.log(process.argv);
+var commands = require('./commands.js');
 
 // Output a prompt
 process.stdout.write('prompt > ');
@@ -6,13 +6,7 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
-  if (cmd === 'pwd') {
-    cmd = process.argv[1];
-  }
-  else if (cmd === 'date') {
-   cmd = new Date().toString();
-  }
+  cmd = commands.shellCommands(cmd);
   process.stdout.write(cmd);
   process.stdout.write('\nprompt > ');
 
--- commands.js
+++ commands.js
@@ -1,1 +1,8 @@
-"test"
+exports.shellCommands = function(cmd) {
+  if (cmd === 'pwd') {
+    cmd = process.argv[1];
+  } else if (cmd === 'date') {
+    cmd = new Date().toString();
+  }
+  return cmd;
+}
