49fe2d599a4d0579979ab1d0bd580d1664cb4281
Kevin Ho <ho.kevins@gmail.com>
2017-11-06 14:33:48 -0500

    EOD 2017-11-06

--- bash.js
+++ bash.js
@@ -6,8 +6,17 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-  cmd = commands.shellCommands(cmd);
-  process.stdout.write(cmd);
-  process.stdout.write('\nprompt > ');
+  // cmd = commands.shellCommands(cmd);
+  var userCommand;
+  if (cmd === 'pwd') {
+    process.stdout.write(commands[cmd]());
+  } else if (cmd === 'date') {
+    process.stdout.write(commands[cmd]());
+  } else if (cmd === 'ls') {
+    commands[cmd]();
+  } else if (cmd.split(' ')[0] === 'echo') {
+    process.stdout.write(commands["echo"](cmd));
+  }
 
+  process.stdout.write('\nprompt > ');
 });
--- commands.js
+++ commands.js
@@ -1,8 +1,46 @@
-exports.shellCommands = function(cmd) {
-  if (cmd === 'pwd') {
-    cmd = process.argv[1];
-  } else if (cmd === 'date') {
-    cmd = new Date().toString();
+// exports.shellCommands = function(cmd) {
+//   if (cmd === 'pwd') {
+//     cmd = process.argv[1];
+//   } else if (cmd === 'date') {
+//     cmd = new Date().toString();
+//   }
+//   return cmd;
+// }
+
+var fs = require('fs');
+
+module.exports = {
+  pwd: function(input) {
+    return process.argv[1];
+  },
+
+  date: function(input) {
+    return new Date().toString();
+  },
+
+  ls: function(input) {
+    fs.readdir('.', function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      })
+      process.stdout.write("prompt > ");
+    });
+  },
+
+  echo: function(input) {
+    return input.split(' ').slice(1).join(' ');
   }
-  return cmd;
+
+  // ls: function() {
+  //   fs.readdir('.', function(err, files) {
+  //     if (err){
+  //       throw err;
+  //     }
+  //     files.forEach(function(file) {
+  //       console.log(file);
+  //       return file.toString() + "\n";
+  //     });
+  //   });
+  // }
 }
